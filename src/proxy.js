ed46a4bc64ad54f840c57fecc7a4719809809878
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-05-15 08:53:45 +0600

    feat: add proxy function for session management and configure route matching

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,7 @@
-import { auth } from '@/lib/auth'
+
 import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
+import { auth } from './lib/auth'
  
 // This function can be marked `async` if using `await` inside
 export async function proxy(request) {
@@ -20,5 +21,5 @@ export async function proxy(request) {
  
 export const config = {
 //   matcher: '/about/:path*',
-  matcher: ['/my-bookings','/add-destination','destinations/:path*'],
+  matcher: ['/my-bookings','/add-destination','/destinations/:path*'],
 }
